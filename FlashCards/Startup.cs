b3577557d0ff4a697af55b641364944a9d7aa16c
Fiyaz Bin Hasan <fiyazhasan@live.com>
2021-05-17 16:22:36 +0600

    Added basic application skeleton using Mud Blazor

--- FlashCards/Startup.cs
+++ FlashCards/Startup.cs
@@ -1,4 +1,3 @@
-using FlashCards.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -22,17 +21,13 @@ namespace FlashCards
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>(); 
             services.AddMudServices();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
