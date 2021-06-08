232c770bbe6761889ad33aefd1477c6196a1ffdd
Fiyaz Bin Hasan <fiyazhasan@live.com>
2021-06-08 14:34:15 +0600

    remove null checking from reducer

--- FlashCards/Store/DecksUseCase/Reducers.cs
+++ FlashCards/Store/DecksUseCase/Reducers.cs
@@ -10,6 +10,6 @@ namespace FlashCards.Store.DecksUseCase
         public static DecksState ReduceAddDeckAction(DecksState state, AddDeckAction action) => state with { Decks = state.Decks.Concat(new Deck[] { action.Deck }) };
 
         [ReducerMethod]
-        public static DecksState ReduceGetDecksAction(DecksState state, GetDecksAction action) => state with { Decks = action.Decks ?? Array.Empty<Deck>() };
+        public static DecksState ReduceGetDecksAction(DecksState state, GetDecksAction action) => state with { Decks = action.Decks };
     }
 }
